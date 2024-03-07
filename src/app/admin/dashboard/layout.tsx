4adc937f13d65b751fcfad528ca5e86d91c7517f
Gokulsck <gvarunblake@gmail.com>
2024-03-06 22:26:56 -0500

    template added

---
+++ src/app/admin/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+export default function DashboardLayout({children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
+return (
+    <>
+      <DefaultLayout>
+        {children}
+      </DefaultLayout>
+    </>
+  );
+}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -24,6 +24,35 @@ export const authOptions = {
                 return null
             },
         }),
+        CredentialsProvider({
+            id: 'custom-signup',
+            name: 'For New Users',
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith@example.com" },
+                password: { label: "Password", type: "password" },
+                confirmPassword: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+
+                // console.log("credentials", credentials)
+                let client = supabase();
+                let { username: email, password, confirmPassword }: any = credentials;
+
+                if (password !== confirmPassword) {
+                    // Password and confirm password incorrect
+                    return null
+                } else if (email && password && confirmPassword) {
+
+                    let { data, error } = await client.auth.signUp({ email, password });
+                    if (data?.user) {
+                        return data.user
+                    }
+                }
+                return null
+
+
+            },
+        }),
     ],
 }
 
@@ -34,7 +63,9 @@ const callbacks = {
 const handler = NextAuth({
     ...authOptions, callbacks: {
         signIn({ user }) {
+            console.log("signIn callback user", user)
             return '/admin/dashboard'
+            // return true
         },
     }
 })
